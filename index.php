c92f1f36083a0d4b8e3b1dd17a7d4332a151bb7e
Malware <85979829+Sploder-Saptarshi@users.noreply.github.com>
2025-10-28 21:41:17 +0530

    Add initial page

--- index.php
+++ index.php
@@ -1,1 +1,109 @@
-Here's to new beginnings!
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Welcome to Zanpo, Virtual Cities: The Online City Simulation Multiplayer Game - MMORPG</title>
+<meta http-equiv="expires" content="0" />
+<meta http-equiv="Pragma" content="no-cache" />
+<link rev="made" href="mailto:info@zanpo.com" />
+<meta name="keywords" content="build, city, virtual city, legos, building blocks, build a city, construct, create, demolish, isometric, 3d, pixel city, lego blocks, lincoln logs, virtual world, playstation, xbox, dvd, mmorpg" />
+<meta name="description" content="Zanpo is a world of 3d multiplayer virtual cities where you can explore, become a citizen, build your own home, and even become mayor of your own city. Become a citizen today!" />
+<meta name="copyright" content="Geoffrey P. Gaudreault 2003" />
+<meta name="Robot" content="FOLLOW" />
+<meta name="rating" content="General" />
+<meta name="DC.Creator" content="Geoffrey P. Gaudreault" />
+<link rel="alternate" type="application/rss+xml" title="RSS" href="/php/citynews.php" />
+<link rel="stylesheet" type="text/css"  href="content/mainpage.css" />
+<!-- Add Ruffle -->
+<script src="https://unpkg.com/@ruffle-rs/ruffle@latest/web/ruffle.js"></script>
+<script type="text/javascript">
+_uacct = "UA-92552-2";
+urchinTracker();
+</script>
+<script language="JavaScript" type="text/javascript">
+var popupwin;
+function closepop()
+{
+	if (popupwin != null)
+	{
+		if (popupwin.closed == false)
+			popupwin.close();
+	}
+};
+</script>
+<link rel="stylesheet" type="text/css"  href="content/style.css" />
+<style type="text/css" media="screen">
+object p
+	{
+	display: none;
+	}
+</style>
+</head>
+<body bgcolor="#3e2349" background="chrome/html-bkgd01.gif" text="#FFFFFF" link="#9999CC" vlink="#666699" style="margin-top: 5px;" onload="" onfocus="closepop();">
+<table align="center" cellpadding="0" cellspacing="0" border="0" width="770">
+	<tr>
+		<td align="right">
+<br /><br />
+		</td>
+	</tr>
+  <tr valign="top">
+    <td align="left" colspan="2"><img align="left" hspace="0" vspace="0" src="chrome/maintitle.gif" border="0" width="770" height="36" alt="Zanpo" /></td>
+  </tr>
+  <tr valign="top">
+    <td align="left"><object align="right" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"
+ codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0"
+ id="zanpo" width="760" height="470">
+        <param name="movie" value="zanpo.swf" />
+        <param name="quality" value="low" />
+        <param name="bgcolor" value="#666666" />
+		<param name="AllowScriptAccess" value="always" />
+	           <embed align="left" src="zanpo.swf" quality="high" bgcolor="#666666" width="760" height="470" swLiveConnect="true" allowScriptAccess="always" name="zanpo"  type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" border="0" hspace="0" vspace="0"></embed>
+		<p>Zanpo is a SimCity-like online Flash game.  If you're interested in simulation or <b>MMORPG</b> games like SimCity or the World of Warcraft, check this out.  Like building things with legos?  Enjoy construction games and
+		toys where you can use your imagination and build?  Sure, console video games like <b>Playstation</b> or <b>Xbox 360</b> and <b>Gameboy</b> are great fun, but the internet allows us to create new kinds 
+		of social software where we can build interactive communities.  A city is the perfect manifestation of that.  So, if you want to
+		play a cool game where you can explore and help build a city, then join us!  But, you need the Flash plug-in first! No <b>joystick</b> necessary!</p>
+      </object>
+    </td>
+    <td align="left"><img src="chrome/shim.gif" width="10" height="10" alt="" /><br />
+      <img  src="chrome/black.gif" width="10" height="460" alt="" /></td>
+  </tr>
+  <tr valign="top">
+    <td colspan="2" align="left"><img src="chrome/shim.gif" width="10" height="10" alt="" /><img src="chrome/black.gif" width="760" height="10" alt="" /></td>
+  </tr>
+</table>
+<center>
+  <table align="center" class="mainnav" width="760" border="0" cellspacing="2" cellpadding="3">
+    <tr>
+      <td align="center" class="flashnav">
+	  	<a class="fhead" href="cities/index.php" target="_blank">Cities</a>&nbsp;&nbsp; &middot; &nbsp;
+		<a class="fhead" style="color: #ff0;" href="php/makeacity.php" target="_blank"><strong>Make Your Own City</strong></a>&nbsp;&nbsp; &middot; &nbsp;
+		<a class="fhead" href="cities/citymanager.php" target="_blank">City Manager</a>&nbsp;&nbsp; &middot; &nbsp;
+		<a class="fhead" href="content/buildingtips.html" target="_blank">Building Tips</a>&nbsp;&nbsp; &middot; &nbsp;
+		<a class="fhead" href="3d/" target="_blank">3d VRML City</a>&nbsp;&nbsp; &middot; &nbsp;
+		<a class="fhead" href="latest_submissions.php" target="_blank">Latest Blocks &raquo;</a>&nbsp;&nbsp;
+		<br /><br />
+	  </td>
+	</tr>
+	  <!--<tr>
+		<td align="center" style="border: 1px solid #ffec00; background: #554488; color: #fff; padding: 10px; font-size: 11px;">
+			Zanpo is undergoing backend server upgrades. Please contact me via the Contact Us link below if you run into any bugs.
+		</td>
+	  </tr>
+	  -->
+	<tr>
+		<td align="center">
+			<p><a href="/privacypolicy.html">Privacy Policy</a> &nbsp; <a href="/termsofservice.html">Terms of Service</a> &nbsp; <a href="contact.php">Contact Us</a></p>
+			<p align="center"><a class="fhead" href="/php/citynews.php"><img src="chrome/rss_icon.gif" border="0" alt="City News RSS Feed" /> City News</a>
+    &nbsp;&nbsp;<a class="fhead" href="/php/submissionsnews.php"><img src="chrome/rss_icon.gif" border="0" alt="Submissions RSS Feed" /> Submissions</a></p>
+		</td>
+	</tr>
+	<tr>
+		<td><br />
+
+		<p align="center"><font face="Arial, Helvetica, Sans-serif" color="#666699" size="1">Zanpo.com
+        Copyright &copy; 2003 <a href="http://www.gaudreault.org">Geoff Gaudreault</a>. All Rights Reserved. Music by <a href="http://www.wetsoul.com">WetSoul</a><br />
+        <a href="mailto: emperor@">contact: zanpo.emperor [at] gmail.com</a> - thanks for playing this multiplayer game.</font></p><br />
+		</td>
+    </tr>
+  </table>
+</center>
+</body>
+</html>
