78aea12c5e0a2c6b2733753733de92604c1bc94c
Malware <85979829+Sploder-Saptarshi@users.noreply.github.com>
2025-10-31 21:26:57 +0530

    Valid stubs for submissions

---
+++ src/php/getsubmissions.php
@@ -0,0 +1,9 @@
+<?php
+header('Content-Type: text/xml');
+echo '<submissions total="5">';
+echo '<submission block_id="242907" user_id="469653" city_id="284" city_name="Pittsburgh" file_name="../../../php/getblock.php" block_name="Modern+Housing" x_location="-4" y_location="7" creation_date="' . date("m.d.Y") . '" link_url="" link_desc="" vote_avg="0" total_votes="0" />';
+echo '<submission block_id="242916" user_id="469653" city_id="183" city_name="Forever+City" file_name="cb_3_-17.xml" block_name="Walls" x_location="3" y_location="-17" creation_date="' . date("m.d.Y") . '" link_url="" link_desc="" vote_avg="0" total_votes="0" />';
+echo '<submission block_id="242918" user_id="469653" city_id="183" city_name="Forever+City" file_name="cb_14_-2.xml" block_name="City+Hall" x_location="14" y_location="-2" creation_date="' . date("m.d.Y") . '" link_url="" link_desc="" vote_avg="0" total_votes="0" />';
+echo '<submission block_id="242919" user_id="469653" city_id="151" city_name="Ravenwood" file_name="cb_5_12.xml" block_name="Park" x_location="5" y_location="12" creation_date="' . date("m.d.Y") . '" link_url="" link_desc="" vote_avg="0" total_votes="0" />';
+echo '<submission block_id="242920" user_id="469653" city_id="245" city_name="Glosoli" file_name="cb_8_-3.xml" block_name="Residential+Area" x_location="8" y_location="-3" creation_date="' . date("m.d.Y") . '" link_url="" link_desc="" vote_avg="0" total_votes="0" />';
+echo '</submissions>';
